34967756409bf84c700557f2035a14607a5ff1eb
Miyu1008 <yamori304@gmail.com>
2024-01-27 21:34:27 +0900

    kadia_013tuika

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,10 +1,12 @@
-# 与えられた引数$priceに送料を加算し、その値を出力する関数を定義する
-def calculate_total(price,shipping_fee):
+def calculate_total(price, shipping_fee):
     # 与えられた引数priceに送料を加算し、変数totalに代入する
-    total = price + (price * shipping_fee)/100
+    total = price + (price * shipping_fee) / 100
 
-    # 変数totalの値を出力する
-    print(f"{total}円")
+    # 変数totalの値を戻り値として返す
+    return total
 
-# 関数を呼び出し、引数として購入金額を渡す
-calculate_total(1200,10)
+# 関数を呼び出し、引数として購入金額と送料を渡す
+total_price = calculate_total(1200, 10)
+
+# 戻り値を出力する
+print(f"{total_price}円")
