618c3568ebf7ab2632aadcae0bf5e452c07f2222
Miyu1008 <yamori304@gmail.com>
2024-01-27 19:24:23 +0900

    iroiro

--- kadai_008/kadai_008.py
+++ kadai_008/kadai_008.py
@@ -1,6 +1,10 @@
 num = 77
 var = 2
 
+#3の倍数かつ5の倍数
+if num % 15 ==0:
+    print("FizzBuzz")
+
 #3の倍数
 if num % 3 ==0:
     print("Fizz")
@@ -8,10 +12,6 @@ if num % 3 ==0:
 #5の倍数
 if num % 5 ==0:
     print("Buzz")
-
-#3の倍数かつ5の倍数
-if num % 15 ==0:
-    print("FizzBuzz")
 
 #該当なし
 else:
